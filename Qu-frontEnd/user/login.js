312dde5717f98d902e65123d75e14613e1a3bb2a
ibrahim <al-edanee@hotmail.com>
2024-05-06 15:34:29 +0300

    fronEnd

---
+++ Qu-frontEnd/user/login.js
@@ -0,0 +1,20 @@
+function login() {
+    var email = document.querySelector(".email-input").value;
+    var password = document.querySelector(".password-input").value;
+
+    // Perform validation or send data to server for authentication
+
+    // For demonstration purpose, just alert the input values
+    alert("Email: " + email + "\nPassword: " + password);
+}
+
+function forgetPassword() {
+    // Add your forget password functionality here
+    alert("Forget Password clicked!");
+}
+
+function signUp() {
+    // Add your sign up functionality here
+    alert("Sign Up clicked!");
+}
+
